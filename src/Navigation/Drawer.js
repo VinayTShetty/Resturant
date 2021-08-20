19dc0b8246d2d223f65000fa141782ef0d104e2d
Vinay T S <vinaytshetty@gmail.com>
2021-08-20 13:15:53 +0530

    working code commit.

--- src/Navigation/Drawer.js
+++ src/Navigation/Drawer.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { About, Contatct, AboutApp } from '../Screens'
-
 const Drawer = createDrawerNavigator();
-
 export default DrawerNavigator = () => {
     return(
         <Drawer.Navigator
-            drawerPosition= "right"
-        >
-            <Drawer.Screen name="About" component={About} />
+            drawerPosition= "right" >
             <Drawer.Screen name="Contact" component={Contatct} />
             <Drawer.Screen name="AboutApp" component={AboutApp} />
+            <Drawer.Screen name="About" component={About} />
         </Drawer.Navigator>
     )
 }
