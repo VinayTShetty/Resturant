95a376457e4d76e50702f6b2f84c2b577f0f7162
Vinay T S <vinaytshetty@gmail.com>
2021-08-20 13:17:18 +0530

    Showing the list of Names in the screen on the contact list is completed.

--- src/Screens/Contact.js
+++ src/Screens/Contact.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// import React from 'react';
+// import { View, Text } from 'react-native';
 
-export default Contacts = () => {
-    return(
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Contacts Screen Vinay T Shetty Git</Text>
-        </View>
-    )
+// export default Contacts = () => {
+//     return(
+//         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//             <Text>Contacts Screen Vinay T Shetty Git</Text>
+//         </View>
+//     )
+// }
+
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
 }
+
+export default FlatListBasics;
